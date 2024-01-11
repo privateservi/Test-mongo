15a5e4f562306cdef0c35600bf7e7b7fe8746036
Samar khan <149138560+SMD-BOTz-Hub@users.noreply.github.com>
2024-01-11 23:53:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1412,13 +1412,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🎋𝐁𝐚𝐜𝐤🍁', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        reply_markup = InlineKeyboardMarkup(buttons)
-    m = await message.reply_sticker("CAACAgUAAxkBAAEBE9NlnW_99bth-pKCUp7xpRuA_x3dbQACjQgAAlRUgVTJ8HhzKcWHyh4E")
-    await asyncio.sleep(1)
-    await m.delete()
-    await message.reply_photo(
-        photo=random.choice(PICS)
-    )
+        await query.message.edit_text(
+            text="● ◌ ◌"
+        )
         await query.message.edit_text(
             text="● ● ◌"
         )
