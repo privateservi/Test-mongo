52c984483a80de82d141c734511620bc1c6b6d0a
Samar khan <149138560+SMD-BOTz-Hub@users.noreply.github.com>
2024-01-11 23:37:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1407,22 +1407,34 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
     
-elif query.data == "source":
-    buttons = [[
-        InlineKeyboardButton('🍁𝐁𝐚𝐜𝐤🎋', callback_data='about')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
+    elif query.data == "source":
+        buttons = [[
+            InlineKeyboardButton('🎋𝐁𝐚𝐜𝐤🍁', callback_data='about')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        reply_markup = InlineKeyboardMarkup(buttons)
     m = await message.reply_sticker("CAACAgUAAxkBAAEBE9NlnW_99bth-pKCUp7xpRuA_x3dbQACjQgAAlRUgVTJ8HhzKcWHyh4E")
     await asyncio.sleep(1)
     await m.delete()
     await message.reply_photo(
         photo=random.choice(PICS)
     )
-    await query.message.edit_text(
-        caption=script.SOURCE_TXT,
-        reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML
-    )
+        await query.message.edit_text(
+            text="● ● ◌"
+        )
+        await query.message.edit_text(
+            text="● ● ●"
+        )
+        await client.edit_message_media(
+            query.message.chat.id, 
+            query.message.id, 
+            InputMediaPhoto(random.choice(PICS))
+        )
+        await query.message.edit_text(
+            text=script.SOURCE_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
     elif query.data == "extra":
         buttons = [[
             InlineKeyboardButton('⚙ 𝐀𝐝𝐦𝐢𝐧 𝐎𝐧𝐥𝐲​ ⚙', callback_data='admin')
